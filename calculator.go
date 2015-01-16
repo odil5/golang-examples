e6fd17a94c1ba34e4027bd3de43df1bf98e3db11
odil5 <mailbitrix@gmail.com>
2015-01-16 15:14:31 +0500

    Update calculator.go

--- calculator.go
+++ calculator.go
@@ -13,7 +13,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 		"<form>" +
 		"a=<input type='text' name='a'><br>" +
 		"b=<input type='text' name='b'> <br> " +
-		"<input type='submit'> " +
+		"<input type='submit'> nimadur " +
 		"</form>"
 	header := "<html>"
 	footer := "</html>"
